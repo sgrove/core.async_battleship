a4dca8f55f38c64ebac7e90bf70d1b6b51b1563a
Sean Grove <s@bushi.do>
2013-10-20 20:51:10 -0700

    Cleanup project.clj

--- project.clj
+++ project.clj
@@ -7,13 +7,12 @@
             [lein-ring "0.8.7"]]
   :dependencies [[com.cemerick/piggieback "0.1.0"]
                  [compojure "1.1.5"]
-                 [prismatic/dommy "0.1.2"]
                  [hiccup "1.0.4"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1934"]
                  [org.clojure/core.async "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.reader "0.7.9"]
+                 [prismatic/dommy "0.1.2"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-json "0.2.0"]
                  [ring/ring-jetty-adapter "1.2.0"]]
@@ -38,8 +37,7 @@
                            :output-to "resources/private/js/unit-test.js",
                            :optimizations :whitespace
                            ;:source-map "resources/public/js/bin-debug/main.map"
-                           }
-                }
+                           }}
                {:source-paths ["src/cljs"],
                 :id "prod",
                 :compiler {:pretty-print false,
